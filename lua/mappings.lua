30cf88732fcec9a36f2995f16d6df0835e882d40
LouisPetrik <33809479+LouisPetrik@users.noreply.github.com>
2024-12-27 09:43:06 +0100

    added telescope on start up

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -10,3 +10,10 @@ map("i", "jk", "<ESC>")
 -- Add Command + S mapping (save file, usually :w)
 map("n", "<D-s>", ":w<CR>")  -- Normal mode
 map("i", "<D-s>", "<ESC>:w<CR>")  -- Insert mode
+
+-- Mappings for telescope 
+map("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Find Files" })
+map("n", "<leader>fg", ":Telescope live_grep<CR>", { desc = "Live Grep" })
+map("n", "<leader>fb", ":Telescope buffers<CR>", { desc = "Find Buffers" })
+map("n", "<leader>fh", ":Telescope help_tags<CR>", { desc = "Find Help Tags" })
+
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,6 +13,24 @@ return {
     end,
   },
 
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        require("telescope").setup {
+            defaults = {
+                mappings = {
+                    i = {
+                        ["<C-u>"] = false,
+                        ["<C-d>"] = false,
+                    },
+                },
+            },
+        }
+    end,
+}
+
+
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
