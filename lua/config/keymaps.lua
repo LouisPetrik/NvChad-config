2173c8592e2a482770227ba57b6fad8e7d5a852b
LouisPetrik <louispetrik@aol.com>
2025-05-15 21:54:49 +0200

    added ctrl + n shortcut for oil.nvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,6 @@ vim.g.maplocalleader = "\\"
 -- Up, Down
 vim.keymap.set('n', 'j', 'gj', { desc = 'Up', noremap = true})
 vim.keymap.set('n', 'k', 'gk', { desc = 'Down', noremap = true})
+
+-- Add this keybinding to open the oil.nvim file tree
+vim.keymap.set("n", "<C-n>", ":Oil<CR>", { noremap = true, silent = true, desc = "Open Oil File Tree" })
